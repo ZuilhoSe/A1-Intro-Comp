1318f41049f694e9f98fd6b842cb6c53c7614680
Zuilho Segundo <segundozuilho@gmail.com>
2022-04-13 09:11:22 -0300

    Testes na aula

--- arquivos.py
+++ arquivos.py
@@ -1,5 +1,6 @@
 import sys
 import comandos
+import re
 
 #Função que lê o arquivo de texto e retorna as linhas do arquivo em forma de lista;
 def ler_arquivo_texto(arquivo):
@@ -8,20 +9,16 @@ def ler_arquivo_texto(arquivo):
     return linhas
 
 #Função que lê o arquivo de comandos e retorna os comandos em forma de lista, todos em letras minúsculas;
-def ler_arquivo_comandos(arquivo):
+def ler_arquivo_comandos(arquivo, encoding="cp1252"):
     arq = open(arquivo) 
     linhas = arq.readlines()
-
-    #Iterando em cima de cada linha para pegar cada comando especifico;
+    
     lista_de_comandos = []
-    for linha in linhas:
-        linha = linha.lower()
-        palavras = linha.split(" ")
-        lista_de_comandos.extend(palavras)
-        
-    #Um problema que pode acontecer é se tiver um enter entre as palavras que aparece um \n, aqui tratamos isso;
-    for i in range(len(lista_de_comandos)):
-        tem_enter = lista_de_comandos[i].endswith("\n")
-        if tem_enter == True:
-            lista_de_comandos[i] = lista_de_comandos[i].replace('\n', '')
+
+    #Aqui temos uma expressão em RegEx para procurar cada um dos comandos no texto. Cada expressão terá um regex próprio 
+    for i in range(len(linhas)):
+        if re.search("Agrupar|Maior|Preguiça", linhas[i]):
+            lista_de_apagar = re.findall("Agrupar\s|Maior\s|Preguiça", linhas[i])
+            lista_de_comandos.append(lista_de_apagar)
+
     return lista_de_comandos
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import arquivos
 import comandos
+import re
 
 lista_de_comandos = arquivos.ler_arquivo_comandos("comandos.txt")
 lista_de_linhas = arquivos.ler_arquivo_texto("texto.txt")
+
+print(lista_de_comandos)
